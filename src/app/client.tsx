017cc3e67d809bed225dd8bbb3cca2f8cd1ce380
Luke Barrier <lukebbarrier06@outlook.com>
2024-11-05 00:34:37 -0500

    Update

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -1,9 +1,12 @@
 'use client';
 
 import { useAPI } from '@/hooks/use-api';
+import { useRouter } from 'next/navigation';
 
 export default function Client() {
     const [SERVER, setHeaders] = useAPI();
+
+    const router = useRouter();
 
     return (
         <>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import { API } from '@/lib/api';
 import Client from './client';
 
 export default async function Page() {
-    const SERVER = await API();
-    const { status } = await SERVER.status.query();
+	const SERVER = await API();
+	const { status } = await SERVER.status.query();
 
-    return (
-        <div>
-            <h1 className="text-3xl font-extrabold">{status}</h1>
+	return (
+		<div>
+			<h1 className="text-3xl font-extrabold">{status}</h1>
 
-            <Client />
-        </div>
-    );
+			<Client />
+		</div>
+	);
 }
